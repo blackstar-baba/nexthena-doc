6cde25903ff0a3f5ad554c9bbfbf57bfd6ec85e5
blackstar-baba <535650957@qq.com>
2025-12-09 12:40:22 +0800

    feat: update copyright

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -140,7 +140,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Nexthena, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
